5c10c0729c809521eb526d723de88a015833ceb1
John Puddephatt <john@jdp.org.uk>
2025-06-11 11:24:35 +0100

    underline

--- resources/views/partials/cookies.blade.php
+++ resources/views/partials/cookies.blade.php
@@ -1,7 +1,8 @@
 <div class="cookie-notice  fixed bottom-0 left-0 right-0 z-20  bg-beige py-3 shadow">
     <div class="container flex items-center gap-3">
-        <p class="font-medium leading-none">
-            {{ get_field('privacy_banner', 'option') }} <a class="underline" href=" {{ get_privacy_policy_url() }}">Find
+        <p class="font-medium leading-none pt-1">
+            {{ get_field('privacy_banner', 'option') }} <a class="!underline  !decoration-black"
+                href=" {{ get_privacy_policy_url() }}">Find
                 out more</a>
         </p>
         <div class="ml-auto flex flex-col-reverse gap-1 md:flex-row">
