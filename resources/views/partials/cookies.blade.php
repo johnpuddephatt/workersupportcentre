e8ffee68e90f7ea4e952121f0a2820dcb6217f10
John Puddephatt <john@jdp.org.uk>
2025-06-11 11:22:31 +0100

    people

--- resources/views/partials/cookies.blade.php
+++ resources/views/partials/cookies.blade.php
@@ -1,14 +1,14 @@
-<div class="cookie-notice border-t fixed bottom-0 left-0 right-0 z-20 border-black bg-beige py-6 shadow">
+<div class="cookie-notice  fixed bottom-0 left-0 right-0 z-20  bg-beige py-3 shadow">
     <div class="container flex items-center gap-3">
-        <p class="font-medium">
+        <p class="font-medium leading-none">
             {{ get_field('privacy_banner', 'option') }} <a class="underline" href=" {{ get_privacy_policy_url() }}">Find
                 out more</a>
         </p>
         <div class="ml-auto flex flex-col-reverse gap-1 md:flex-row">
             <button
-                class="decline-cookies  block  border-2 border-black px-6 py-2 font-semibold !no-underline transition duration-300 hover:bg-green hover:bg-opacity-20">Decline</button>
+                class="decline-cookies  leading-none block  border-2 border-black px-6 py-2 font-semibold !no-underline transition duration-300 hover:bg-green hover:bg-opacity-20">Decline</button>
             <button
-                class="accept-cookies block  border-2 border-black bg-lime px-12 py-2 font-semibold text-black !no-underline transition duration-300">Accept</button>
+                class="accept-cookies block leading-none  border-2 border-black bg-lime px-12 py-2 font-semibold text-black !no-underline transition duration-300">Accept</button>
         </div>
     </div>
 </div>
