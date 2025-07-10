793088cb4d76fd8bcb37c5464eb177f3392422be
John Puddephatt <john@jdp.org.uk>
2025-07-10 14:38:42 +0100

    icon

--- resources/views/blocks/text-icon.blade.php
+++ resources/views/blocks/text-icon.blade.php
@@ -1,7 +1,9 @@
 <div class="wp-block container flex my-12 gap-4 {{ $block->classes }} items-start">
     <div
         class="{{ match ($block->style) {'beige' => 'bg-beige','mint' => 'bg-mint','peach' => 'bg-peach','orange' => 'bg-orange text-white','teal' => 'bg-teal text-white',default => 'bg-beige'} }} rounded-full p-4  ">
-        @svg($icon, 'w-8 h-8')
+        @if ($icon)
+            @svg($icon, 'w-8 h-8')
+        @endif
 
     </div>
 
