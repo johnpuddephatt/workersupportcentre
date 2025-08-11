850907aefbfd90cebb1143db8cefafbcc83df52b
John Puddephatt <john@jdp.org.uk>
2025-08-11 22:40:19 +0100

    list style position

--- resources/views/blocks/text-icon.blade.php
+++ resources/views/blocks/text-icon.blade.php
@@ -1,4 +1,4 @@
-<div class="wp-block container flex my-12 gap-4 {{ $block->classes }} items-start">
+<div class="wp-block container flex my-6 lg:my-12 gap-4 {{ $block->classes }} items-start">
     <div
         class="{{ match ($block->style) {'beige' => 'bg-beige','mint' => 'bg-mint','peach' => 'bg-peach','orange' => 'bg-orange text-white','teal' => 'bg-teal text-white',default => 'bg-beige'} }} rounded-full p-4  ">
         @if ($icon)
