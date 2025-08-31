ee324c4d4ec29784c59f3b8e25e17aa68c9dc08e
John Puddephatt <john@jdp.org.uk>
2025-08-31 12:59:57 +0100

    order

--- resources/views/blocks/text-accordion.blade.php
+++ resources/views/blocks/text-accordion.blade.php
@@ -1,9 +1,5 @@
 <div
     class="wp-block container grid lg:grid-cols-2  my-6 lg:my-12 gap-4 {{ $block->classes }} {{ $align == 'center' ? 'items-center' : '' }}">
-
-    <div class="prose border-l border-lime pl-4 lg:pl-8">
-        <InnerBlocks template="{{ $block->template }}" />
-    </div>
     <div class="{{ $block->style == 'flipped' ? 'order-2' : '' }}">
 
         @foreach ($accordion as $accordionItem)
@@ -15,4 +11,8 @@
             </details>
         @endforeach
     </div>
+    <div class="prose border-l border-lime pl-4 lg:pl-8">
+        <InnerBlocks template="{{ $block->template }}" />
+    </div>
+
 </div>
