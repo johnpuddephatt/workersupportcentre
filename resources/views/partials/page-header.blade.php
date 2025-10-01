657edd5cd03b9f62889a7f27ffa276697e63319c
John Puddephatt <john@jdp.org.uk>
2025-10-01 14:04:37 +0100

    page-header on mobile

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,9 +1,9 @@
-<div class="wp-block  bg-beige mb-8 overflow-hidden relative py-16 not-prose">
-    <div class="container alignwide">
+<div class="wp-block  bg-beige mb-8 overflow-hidden relative lg:py-16 not-prose">
+    <div class="container max-lg:px-0 alignwide">
 
         <div
             class=" relative lg:items-center flex flex-col-reverse  gap-8 lg:gap-12 xl:gap-16 2xl:gap-24 lg:grid lg:grid-cols-2">
-            <div class="">
+            <div class="pb-8 lg:pb-0">
                 <h1 class="type-2xl type-underlined mb-6  md:mb-8 ">
                     {!! $title !!}</h1>
                 <div class=" max-w-md">{!! get_the_excerpt() !!}</div>
