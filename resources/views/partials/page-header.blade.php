537183cd3b212a292b3048266f1ff5e59d0fc781
John Puddephatt <john@jdp.org.uk>
2025-10-01 14:05:40 +0100

    page-header on mobile

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -3,7 +3,7 @@
 
         <div
             class=" relative lg:items-center flex flex-col-reverse  gap-8 lg:gap-12 xl:gap-16 2xl:gap-24 lg:grid lg:grid-cols-2">
-            <div class="pb-8 lg:pb-0">
+            <div class="pb-8 lg:pb-0 px-4 lg:px-0">
                 <h1 class="type-2xl type-underlined mb-6  md:mb-8 ">
                     {!! $title !!}</h1>
                 <div class=" max-w-md">{!! get_the_excerpt() !!}</div>
